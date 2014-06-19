0cce40e688853a03b5a690a5b6f1fd1764d1c422
Jonathan Nesci <grasshoper@gmail.com>
2014-06-19 08:28:16 -0700

    Added my notes.

--- D3book-Notes-v1.js
+++ D3book-Notes-v1.js
@@ -35,5 +35,24 @@ Variable hoisting can account for conflicts between vars in functions.
 Variable declarations are actually hoisted & executed at the top of a function, so not in the order you wrote them necessarily. Be careful.
 */
 
-// --- STOPPED @ CH3.SVG
-// http://chimera.labs.oreilly.com/books/1230000000345/ch03.html#SVG_3
+/*
+Applying styles to SVG elements:
+You can style inline or using CSS styles, like so:
+*/
+//SVG
+<circle cx="25" cy="25" r="22" class="pumpkin"/>
+//CSS
+.pumpkin {
+    fill: yellow;
+    stroke: orange;
+    stroke-width: 5;
+ }
+ // but its better to be clear in your CSS that those styles are for SVG attributes, and are not standard CSS (the above attributes aren't actually part of CSS, they're specific to SVG):
+ svg .pumpkin {
+    /* ... */
+ }
+
+
+
+// --- STOPPED @ CH4
+// http://chimera.labs.oreilly.com/books/1230000000345/ch04.html#_downloading_d3
